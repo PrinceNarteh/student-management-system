d7740e45519196886004fad364895ac4fddd98d8
Prince Narteh <prinarteh@gmail.com>
2024-06-04 07:00:00 -0700

    feat: add entity repository

---
+++ src/database/entity.repository.ts
@@ -0,0 +1,51 @@
+import {
+  Document,
+  FilterQuery,
+  Model,
+  ProjectionType,
+  QueryOptions,
+  UpdateQuery,
+} from 'mongoose';
+
+export abstract class EntityRepository<T extends Document> {
+  constructor(protected readonly entityModel: Model<T>) {}
+
+  async findAll(): Promise<T[]> {
+    return this.entityModel.find();
+  }
+
+  async findOne(
+    filter: FilterQuery<T>,
+    projection: ProjectionType<T>,
+    options: QueryOptions<T>,
+  ): Promise<T | null> {
+    return this.entityModel.findOne(filter, projection, options);
+  }
+
+  async findById(
+    id: string,
+    projection: ProjectionType<T>,
+    options: QueryOptions<T>,
+  ): Promise<T | null> {
+    return this.entityModel.findById(id, projection, options);
+  }
+
+  async create(createEntityData: unknown): Promise<T> {
+    const entity = new this.entityModel(createEntityData);
+    return entity.save();
+  }
+
+  async findOneAndUpdate(
+    filter: FilterQuery<T>,
+    updateEntityData: UpdateQuery<unknown>,
+  ): Promise<T | null> {
+    return this.entityModel.findOneAndUpdate(filter, updateEntityData, {
+      new: true,
+    });
+  }
+
+  async delete(filter: FilterQuery<T>): Promise<boolean> {
+    const deletedResult = await this.entityModel.deleteOne(filter);
+    return deletedResult.deletedCount >= 1 ? true : false;
+  }
+}
